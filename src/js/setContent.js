8b20f0dc26071ab917c359874b6e4e8b3eb7b3b9
Deskri <deskri@mail.ru>
2023-05-01 23:53:53 +0400

    first commit

---
+++ src/js/setContent.js
@@ -0,0 +1,24 @@
+function loadHTML(id, filename) {
+    console.log(`div id: ${id}, filename: ${filename}`);
+
+    let xhttp;
+    let element = document.getElementById(id);
+    let file = filename;
+
+    if (file) {
+        xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    element.innerHTML = this.responseText;
+                }
+                if (this.status == 400) {
+                    
+                }
+            }
+        }
+        xhttp.open("GET", `src/templates/${file}`, true);
+        xhttp.send();
+        return;
+    }
+}
